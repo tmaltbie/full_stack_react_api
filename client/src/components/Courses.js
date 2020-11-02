39116952bd0c66300fe51ff0acc42d92e5e269a5
Trevor Maltbie <60497552+tmaltbie@users.noreply.github.com>
2020-11-02 14:13:09 -0800

    courses updates

--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -25,12 +25,18 @@ export default class Courses extends Component {
     
     render() {
         const courses = this.state.courses
+        // 
         return (
-            <div>
-                <ul>
-                    {courses.map(course => <li>{course.title}</li>)}
-                </ul>
+            <hr>
+            <div className="bounds">
+                {courses.map(course => 
+                    <div className="grid-33"><a className="course--module course--link" href="course-detail.html">
+                        <h4 className="course--label">Course</h4>
+                        <h3 className="course--title">{course.title}</h3>
+                    </a></div>
+                )}
             </div>
+            </hr>
         )
     }
 }
