6981e62216b4f45ad8c01219d9d1e9dd2868538c
Trevor Maltbie <60497552+tmaltbie@users.noreply.github.com>
2020-11-03 09:46:49 -0800

    fixed react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom;'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 import Header from './components/Header'
 import Courses from './components/Courses'
@@ -14,20 +14,22 @@ import UserSignOut from './components/UserSignOut'
 
 
 export default function App() {
-    <Router>
-        <div>
-            <Header />
+    return (
+        <Router>
+            <div>
+                <Header />
 
-            <Switch>
-                <Route exact path="/" component={Courses} />
-                <Route path="/courses/create" component={CreateCourse} />
-                <Route path={'/courses/:id/update'} component={UpdateCourse} />
-                <Route path ={'/courses/:id'} component={CourseDetail} />
+                <Switch>
+                    <Route exact path="/" component={Courses} />
+                    <Route path="/courses/create" component={CreateCourse} />
+                    <Route path={'/courses/:id/update'} component={UpdateCourse} />
+                    <Route path ={'/courses/:id'} component={CourseDetail} />
 
-                <Route path="/signin" component={UsersSignIn} />
-                <Route path="/singup" component={UsersSignUp} />
-                <Route path="/signout" component={UserSignOut} />
-            </Switch>
-        </div>
-    </Router>
+                    <Route path="/signin" component={UsersSignIn} />
+                    <Route path="/singup" component={UsersSignUp} />
+                    <Route path="/signout" component={UserSignOut} />
+                </Switch>
+            </div>
+        </Router>
+    )
 }
